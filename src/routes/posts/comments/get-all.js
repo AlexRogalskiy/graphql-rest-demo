6721bad15af4742663f47f4029112705e5b5c31c
Dustin Schau <dustinschau@gmail.com>
2018-04-28 15:44:42 -0500

    chore: add limit/start to other endpoints

--- src/routes/posts/comments/get-all.js
+++ src/routes/posts/comments/get-all.js
@@ -1,9 +1,10 @@
 import { database } from '../../../database';
 
 export function getAll(req, res) {
-  const comments = database.getCollection('comments');
   const { id } = req.params;
+  const { limit = 100, start = 0 } = req.query;
+  const comments = database.getCollection('comments');
   return res.json({
-    comments: comments.find({ parent: { '$eq': id }})
+    comments: comments.chain().find({ parent: { '$eq': id }}).limit(limit).start(start).data()
   });
 }
--- src/routes/users/get-all.js
+++ src/routes/users/get-all.js
@@ -1,8 +1,9 @@
 import { database } from '../../database';
 
 export function getAll(req, res) {
+  const { limit = 100, start = 0 } = req.query;
   const users = database.getCollection('users');
   return res.json({
-    users: users.find()
+    users: users.chain().find().simplesort('id').offset(start).limit(limit).data()
   });
 }
