4fcad72cd8ea203250af3a192731f4b806afd2ee
Dustin Schau <dustinschau@gmail.com>
2018-04-28 15:41:41 -0500

    chore: some minor tweaks

--- src/schema/user.js
+++ src/schema/user.js
@@ -6,11 +6,5 @@ export const USER = yup.object().shape({
     last: yup.string().required()
   }),
   email: yup.string().email().required(),
-  id: yup.string(),
-  location: yup.object().shape({
-    city: yup.string().required(),
-    state: yup.string().required(),
-    address: yup.string().required(),
-    zip: yup.string().required()
-  })
+  id: yup.string()
 });
